3c9b72eebe960b0e7603ef35c8cb2d07999fd97a
Tony George <tg764562@gmail.com>
2026-01-28 12:35:34 +0200

    feat: add workout modal

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Image } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { NavigationContainer, RouteProp, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { FontAwesome6 } from "@react-native-vector-icons/fontawesome6";
@@ -19,10 +18,15 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 function RootStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Group>
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Workout" component={WorkoutScreen} options={({ route }) => ({
-        title: route.params.title || "Day"
-      })}/>
+          title: route.params.title || "Day"
+        })}/>
+      </Stack.Group>
+      <Stack.Group screenOptions={{presentation: 'modal'}}>
+        <Stack.Screen name='Add Workout' component={AddWorkoutModal}/>
+      </Stack.Group>
     </Stack.Navigator>
   );
 }
@@ -31,30 +35,89 @@ function HomeScreen() {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   let workouts = useContext(WorkoutsContext);
   return (
-    <View style={styles.container}>
-      {days.map(day => <Card title={day} onPress={() => navigation.navigate('Day', { 'title': day })} />)}
-    </View>
+    <>
+      <View style={styles.home}>
+        {workouts.map(workout => <Button key={workout} title={workout} onPress={() => navigation.navigate('Workout', { 'title': workout })} />)}
+      </View>
+      <View style={styles.fab}>
+        <TouchableOpacity onPress={() => navigation.navigate("Add Workout")}>
+          <Text>
+            <FontAwesome6 name="plus" size={30} color="#000" iconStyle='solid' />
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
-function Day() {
+function AddWorkoutModal() {
+  const [workoutName, setWorkoutName] = useState('');
+  const setWorkouts = useContext(SetWorkoutsContext);
+  const navigation = useNavigation();
+  return (
+    <View style={styles.modal}>
+      <View style={styles.container}>
+        <TextInput placeholder='workout name' onChangeText={(text) => setWorkoutName(text)} style={styles.textInput} />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => {
+        if(workoutName.length < 3) return;
+        saveNewWorkout(workoutName).then(workouts => setWorkouts(workouts));
+        navigation.goBack();
+      }}>
+        <View>
+          <Text style={styles.buttonText}>submit</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const saveNewWorkout = async (workoutName: string) => {
+  const days = await AsyncStorage.getItem('days');
+  let workouts = [];
+  if(days != null) {
+    workouts = JSON.parse(days);
+  }
+  workouts.push(workoutName);
+  AsyncStorage.setItem('days', JSON.stringify(workouts));
+
+  return workouts;
+}
+
+function WorkoutScreen({route}: {route: RouteProp<RootStackParamList, 'Workout'>}) {
+  const workout = route.params.title;
   return (
     <View style={styles.container}>
-      <Text>Day Screen</Text>
+      <Text>{workout} Screen</Text>
     </View>
   );
 }
 
-function Card({title, onPress}: {title: string, onPress: () => void}) {
-  return <Pressable style={styles.card} onPress={onPress}>
+function Button({title, onPress}: {title: string, onPress: () => void}) {
+  return <TouchableOpacity style={styles.card} onPress={onPress}>
     <Text>
       {title}
     </Text>
-  </Pressable>;
+  </TouchableOpacity>;
+}
+
+function Card({title, imagePath, onPress}: {title: string, imagePath: string, onPress: () => void}) {
+  return <TouchableOpacity style={styles.card} onPress={onPress}>
+    <View>
+      <Image source={{ uri: imagePath }} style={styles.cardImage} />
+    </View>
+    <Text>
+      {title}
+    </Text>
+  </TouchableOpacity>
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    padding: 1,
+  },
+  home: {
     flex: 1,
     justifyContent: 'center'
   },
@@ -64,6 +127,45 @@ const styles = StyleSheet.create({
     padding: 16,
     margin: 10,
     shadowColor: '#000',
+  },
+  fab: {
+    position: 'absolute',
+    bottom: 70,
+    right: 30,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#fff'
+  },
+  textInput: {
+    width: '95%',
+    margin: 10,
+    backgroundColor: '#373737',
+    color: '#fff',
+    fontSize: 15,
+    borderRadius: 10,
+  },
+  button: {
+    width: '95%',
+    margin: 10,
+    backgroundColor: '#fff',
+    fontSize: 15,
+    borderRadius: 10,
+    textAlign: 'center',
+    padding: 10,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    textAlign: 'center'
+  },
+  modal: {
+    display: 'flex',
+    height: '90%',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  cardImage: {
+    width: 150,
+    height: 75
   }
 });
 
@@ -73,9 +175,9 @@ export default function App() {
     <SafeAreaProvider>
       <WorkoutsContext value={workouts}>
         <SetWorkoutsContext value={setWorkouts}>
-      <NavigationContainer>
-        <RootStack />
-      </NavigationContainer>
+          <NavigationContainer>
+            <RootStack />
+          </NavigationContainer>
         </SetWorkoutsContext>
       </WorkoutsContext>
     </SafeAreaProvider>
