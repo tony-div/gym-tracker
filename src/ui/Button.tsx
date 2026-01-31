6e8d829726134a8c57c4aef4c9af50deb7978d83
Tony George <tg764562@gmail.com>
2026-01-31 05:42:40 +0200

    feat(ui): button more styling & nesting

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,17 +1,35 @@
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { FontAwesome6 } from "@react-native-vector-icons/fontawesome6";
 
-export default function Button({title, onPress, align='start', iconName}: {title?: string, onPress: () => void, align?: 'start' | 'centered', iconName?: string}) {
-  return <TouchableOpacity style={[...(align === 'start'? [styles.button]: [styles.button, styles.centered]), iconName === undefined? styles.buttonNoIcon: {}]} onPress={onPress}>
+export default function Button({
+  title, 
+  onPress, 
+  align='start', 
+  iconName, 
+  iconSize='normal', 
+  children
+  }: {
+    title?: string, 
+    onPress: () => void, 
+    align?: 'start' | 'centered', 
+    iconName?: string, 
+    iconSize?: 'small' | 'normal',
+    children?: React.JSX.Element
+  }) {
+  return <TouchableOpacity style={[styles.button, (align === 'start'? undefined: styles.centered), iconName === undefined? styles.buttonNoIcon: {}]} onPress={onPress}>
     <Text style={styles.buttonText}>
-      {iconName && <FontAwesome6 name={iconName as any} size={30} color="#000" iconStyle='solid' />}
+      {iconName && <FontAwesome6 name={iconName as any} size={iconSize === 'small'? 20: 30} color="#000" iconStyle='solid' />}
       {title}
     </Text>
+    {children}
   </TouchableOpacity>;
 }
 
 const styles = StyleSheet.create({
   button: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     backgroundColor: 'white',
     borderRadius: 10,
     padding: 10,
