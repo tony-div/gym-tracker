0e5b9d5038a7f4e4c284f849e64c4d8b9e038480
Tony George <tg764562@gmail.com>
2026-01-31 08:41:40 +0200

    fix: toolbar style

--- src/ui/ButtonWithToolBar.tsx
+++ src/ui/ButtonWithToolBar.tsx
@@ -1,10 +1,17 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Button from "./Button";
 
 export default function ButtonWitToolBar({title, tools}: {title: string, tools: {iconName: string, onPress: () => void}[]}) {
   return <Button onPress={() => {}} title={title}>
-    <View>
-      {tools.map(tool => <Button key={tool.iconName} iconName={tool.iconName} onPress={tool.onPress} />)}
+    <View style={styles.toolbar}>
+      {tools.map(tool => <Button key={tool.iconName} iconSize="small" iconName={tool.iconName} onPress={tool.onPress} />)}
     </View>
   </Button>
 }
+
+const styles = StyleSheet.create({
+  toolbar: {
+    display: 'flex',
+    flexDirection: 'row'
+  }
+})
