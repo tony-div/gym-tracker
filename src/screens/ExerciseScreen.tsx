64b3733385750b20bb400346d1f11ea4703b8d53
Tony George <tg764562@gmail.com>
2026-02-01 18:26:47 +0200

    fix: exercise screen safe area view

--- src/screens/ExerciseScreen.tsx
+++ src/screens/ExerciseScreen.tsx
@@ -8,40 +8,42 @@ import VideoPlayer from "../ui/VideoPlayer";
 import Button from "../ui/Button";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { Exercise } from "../interfaces/exercise";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ExerciseScreen({route}: {route: RouteProp<RootStackParamList, 'Exercise'>}) {
   const exercise = route.params.exercise;
   const [imagePreviewVisible, setImagePreviewVisible] = useState(false);
   const { width: screenWidth, height: screenHeight } = useWindowDimensions();
   return (
-    <View style={styles.screen}>
-      <View style={styles.scollableContent}>
-
-        <ScrollView>
-          <View style={styles.section}>
-            <Card title="weight" alt={`${exercise.weight} ${exercise.weightUnit} `} />
-            <Card title="sets" alt={`${exercise.sets}`} />
-            <Card title="reps" alt={`${exercise.reps}`} />
-          </View>
-          <View style={styles.section}>
+    <SafeAreaView>
+      <View style={styles.screen}>
+        <View style={styles.scollableContent}>
+          <ScrollView>
+            <View style={styles.section}>
+              <Card title="weight" alt={`${exercise.weight} ${exercise.weightUnit} `} />
+              <Card title="sets" alt={`${exercise.sets}`} />
+              <Card title="reps" alt={`${exercise.reps}`} />
+            </View>
             {exercise.imagePath && (
-              <>
-                <TouchableOpacity onPress={() => setImagePreviewVisible(true)}>
-                  <Image source={{uri: exercise.imagePath}} style={{width: screenWidth * 0.9, height: screenHeight * 0.3}} />
-                </TouchableOpacity>
-                <ImagePreview imageUri={exercise.imagePath} visible={imagePreviewVisible} onRequestClose={() => setImagePreviewVisible(false)} />
-              </>
+              <View style={styles.section}>
+                  <>
+                    <TouchableOpacity onPress={() => setImagePreviewVisible(true)}>
+                      <Image source={{uri: exercise.imagePath}} style={{width: screenWidth * 0.9, height: screenHeight * 0.3}} />
+                    </TouchableOpacity>
+                    <ImagePreview imageUri={exercise.imagePath} visible={imagePreviewVisible} onRequestClose={() => setImagePreviewVisible(false)} readonly />
+                  </>
+              </View>
             )}
-          </View>
-          <View style={styles.section}>
-            {exercise.videoPath && (
-              <VideoPlayer uri={exercise.videoPath} />
-            )}
-          </View>
-        </ScrollView>
+            <View style={styles.section}>
+              {exercise.videoPath && (
+                <VideoPlayer uri={exercise.videoPath} />
+              )}
+            </View>
+          </ScrollView>
+        </View>
+        {/* <Button title="start exercise" onPress={() => {}} align="centered" /> */}
       </View>
-      <Button title="start exercise" onPress={() => {}} align="centered" />
-    </View>
+    </SafeAreaView>
   )
 }
 
@@ -56,10 +58,10 @@ const styles = StyleSheet.create({
   screen: {
     display: 'flex',
     flexDirection: 'column',
-    marginBottom: 10,
   },
   scollableContent: {
-    height: '90%',
+    height: '100%',
+    flexGrow: 1,
   },
   section: {
     display: 'flex',
