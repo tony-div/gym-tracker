3f71a4412129f29d9888e38ba9961573de0ebc80
Tony George <tg764562@gmail.com>
2026-02-01 17:30:44 +0200

    feat(image-preview): delete option

--- src/ui/ImagePreview.tsx
+++ src/ui/ImagePreview.tsx
@@ -1,11 +1,14 @@
 import { Image, Modal, StyleSheet, useWindowDimensions, View } from "react-native";
 import Button from "./Button";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function ImagePreview({visible, onRequestClose, imageUri, pickAnotherHandler}: {
+export default function ImagePreview({visible, onRequestClose, imageUri, pickAnotherHandler, deleteHandler, readonly=false}: {
   visible: boolean;
   onRequestClose: () => void;
   imageUri: string;
   pickAnotherHandler?: () => void;
+  deleteHandler?: () => void;
+  readonly?: boolean;
 }) {
   const { width: screenWidth, height: screenHeight } = useWindowDimensions();
   return (
@@ -14,19 +17,21 @@ export default function ImagePreview({visible, onRequestClose, imageUri, pickAno
       backdropColor={'black'} 
       visible={visible}
       onRequestClose={onRequestClose}
-      style={styles.imageModal}
     >
-      <View style={styles.imageContainer}>
-        <Image resizeMode='contain' source={{uri: imageUri}} width={screenWidth * 0.9} height={screenHeight * 0.7} />
-      </View>
-      <View style={styles.row}>
-        <View style={styles.rowChild}>
-          {pickAnotherHandler && <Button title='pick another photo' onPress={pickAnotherHandler} />}
+      <SafeAreaView style={styles.imageModal}>
+        <View style={styles.imageContainer}>
+          <Image resizeMode='contain' source={{uri: imageUri}} width={screenWidth * 0.9} height={screenHeight * 0.7} />
         </View>
-        <View style={styles.rowChild}>
-          <Button title='ok' onPress={onRequestClose} align="centered" />
+        <View style={styles.row}>
+          <View style={styles.rowChild}>
+            {(readonly === false) && pickAnotherHandler && <Button title='pick another photo' onPress={pickAnotherHandler} />}
+            {(readonly === false) && deleteHandler && <Button title='delete' onPress={deleteHandler} />}
+          </View>
+          <View style={styles.rowChild}>
+            <Button title='ok' onPress={onRequestClose} align="centered" />
+          </View>
         </View>
-      </View>
+      </SafeAreaView>
     </Modal>
   );
 }
